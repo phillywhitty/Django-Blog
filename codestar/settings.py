5ba8a1c62fd1fd8ca69a02327a0295da7c954897
phillywhitty <philipwhitty92@yahoo.ie>
2023-09-14 14:32:36 +0000

    connect elephantsql and create env.py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,12 +26,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'phils_key1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['phils-django-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-phillywhitty-djangoblog-ef5kjr0qwry.ws-eu104.gitpod.io']
 
 
 # Application definition
